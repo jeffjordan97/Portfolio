d5886803d7a6c7ee3ebe38e3dfbc4014d0bcdb86
Jeffrey Jordan <jeffjordan97@hotmail.co.uk>
2024-09-24 17:22:24 +0100

    Auto-generated commit

---
+++ browser/chunk-IK6N3OCH.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-7QN4WM6U.js";import"./chunk-7QAXJ3ZB.js";export{a as AboutComponent};
---
+++ browser/chunk-UZ77QDTP.js
@@ -0,0 +1,1 @@
+import{p as a}from"./chunk-JHGWFY2I.js";import"./chunk-7QAXJ3ZB.js";export{a as SkillsComponent};
